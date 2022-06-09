7fb670317339a0b2cdadb7eb90589ff72404ea4c
Kyle M. Lang <k.m.lang@uu.nl>
2022-06-09 16:28:56 +0200

    finishing first draft of SEM lecture

---
+++ code/install_stuff.R
@@ -0,0 +1,9 @@
+install.packages(c("knitr",
+                   "ggplot2",
+                   "MASS",
+                   "DAAG",
+                   "xtable",
+                   "MLmetrics",
+                   "dplyr",
+                   "mvtnorm"),
+                 repos = "http://cloud.r-project.org")
